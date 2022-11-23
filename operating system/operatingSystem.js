7be919fc5f1f09cec6c40b87eea106ca9e0a44c9
unknown <jlrequenaetcheverria@gmail.com>
2022-11-23 23:42:59 +0100

    description and test data

--- operating system/operatingSystem.js
+++ operating system/operatingSystem.js
@@ -1,5 +1,21 @@
+/* Evelyn Belefzin 👩‍💻 está trabajando en un sistema operativo 
+para ser usado en el taller de Santa Claus 🎅.
+
+Se ha dado cuenta que en el taller nadie le presta atención a 
+los nombres de los ficheros y a veces intentan guardar el mismo 
+fichero más de una vez... así que es importante que gestionemos 
+bien los nombres duplicados.
+
+Tenemos que crear una función que al pasarnos un array de nombres 
+de archivo devolvamos un array con el mismo número de elementos 
+pero donde los nombres que se repetían se anexe al final (k) 
+donde k sería el número de veces que se encontró repetido.
+
+Lo mejor es que veamos un ejemplo:*/
+
+
 function fixFiles(files) {
-    // ¡No olvides compartir tu solución en redes!
+    
     for (let i=0 ; i < files.length ; i++){
         let contadorj = 0;
         for (let j = i+1 ; j <= files.length ; j++){
@@ -11,3 +27,15 @@ function fixFiles(files) {
     }
     return files
 }
+
+
+//test data
+const files = ['photo', 'postcard', 'photo', 'photo', 'video']
+fixFiles(files) // ['photo', 'postcard', 'photo(1)', 'photo(2)', 'video']
+
+const files2 = ['file', 'file', 'file', 'game', 'game']
+fixFiles(files2) = ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
+
+// ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
+const files3 = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
+fixFiles(files3) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
